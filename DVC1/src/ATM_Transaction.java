4df8640dadd071829217837012e1de21f73ddd88
AB <AB@DESKTOP-D09B0CO.mshome.net>
2021-05-04 19:19:49 +0300

    edited one

--- DVC1/src/ATM_Transaction.java
+++ DVC1/src/ATM_Transaction.java
@@ -6,23 +6,25 @@ public class ATM_Transaction
         int balance = 5000, withdraw, deposit;
         Scanner s = new Scanner(System.in);
         while(true)
-        {
-            System.out.println("Automated Teller Machine");
-            System.out.println("Choose 1 for Withdraw");
-            System.out.println("Choose 2 for Deposit");
-            System.out.println("Choose 3 for Check Balance");
-            System.out.println("Choose 4 for EXIT");
+        {    
+            System.out.println("======Automated Teller Machine=====");
+            System.out.println();
+            System.out.println("------Choose 1 For Withdraw--------");
+            System.out.println("------Choose 2 For Deposit---------");
+            System.out.println("------Choose 3 For Check Balance----");
+            System.out.println("------Choose 4 For EXIT-------------");
+            System.out.println();
             System.out.print("Choose the operation you want to perform:");
             int n = s.nextInt();
             switch(n)
             {
                 case 1:
-                System.out.print("Enter money to be withdrawn:");
+                System.out.print("Enter ammount of money to be withdrawn:");
                 withdraw = s.nextInt();
                 if(balance >= withdraw)
                 {
                     balance = balance - withdraw;
-                    System.out.println("Please collect your money");
+                    System.out.println("Withdrawn is Completed, Please Take Your Card!!");
                 }
                 else
                 {
@@ -30,7 +32,7 @@ public class ATM_Transaction
                 }
                 System.out.println("");
                 break;
- 
+               
                 case 2:
                 System.out.print("Enter money to be deposited:");
                 deposit = s.nextInt();
